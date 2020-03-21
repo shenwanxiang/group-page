c4708de00cd8726be3ab4ee3c3f6d1330241da13
Charleshen <senwanxiang@163.com>
2020-03-21 20:27:23 +0800

    Create config_machine.sh

---
+++ config_machine.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# First sudo command
+sudo whoami
+
+# Update and upgrade
+sudo apt update
+sudo apt upgrade -y
+
+# Utility
+
+# Essential
+sudo apt install -y git python3-pip gcc cmake
+
+# Networking tools
+sudo apt install -y curl net-tools
+
+# Networking tools (optional)
+sudo apt install -y openssh-server nmap
+
+## For CUDA
+sudo apt-get install -y freeglut3 freeglut3-dev libxi-dev libxmu-dev gcc-6 g++-6
+
+## VMware Tools (In case of VM)
+sudo apt-get install -y open-vm-tools open-vm-tools-desktop
+
+
+# Nvidia
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+
+
+
+# Add NVIDIA package repositories
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo apt-get update
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt-get update
+
+# Install NVIDIA driver
+sudo apt-get install --no-install-recommends nvidia-driver-430
+# Reboot. Check that GPUs are visible using the command: nvidia-smi
+
+# Install development and runtime libraries (~4GB)
+sudo apt-get install --no-install-recommends \
+    cuda-10-1 \
+    libcudnn7=7.6.4.38-1+cuda10.1  \
+    libcudnn7-dev=7.6.4.38-1+cuda10.1
+
+
+# Install TensorRT. Requires that libcudnn7 is installed above.
+sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 \
+    libnvinfer-dev=6.0.1-1+cuda10.1 \
+    libnvinfer-plugin6=6.0.1-1+cuda10.1
